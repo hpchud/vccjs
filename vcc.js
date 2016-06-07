66925c8ebe0ca3603651a13b16668f40e35a3073
Joshua Higgins <joshiggins@gmail.com>
2016-06-07 17:48:21 +0100

    log found interfaces

--- vcc.js
+++ vcc.js
@@ -4,6 +4,7 @@ var os = require("os");
 var network = require("network");
 var yaml = require("yamljs");
 var winston = require("winston");
+var promise = require("deferred");
 
 var kvstore = require("./kvstore.js");
 
@@ -19,24 +20,24 @@ var logger = new (winston.Logger)({
 });
 
 
-
-
-
-
 function ClusterWatcher (config) {
     // load the config file
     this.config = config;
+    logger.info("ClusterWatcher initialised with config", config);
 }
 
 ClusterWatcher.prototype.getAddress = function () {
     var deferred = promise();
     network.get_interfaces_list(function (err, list) {
         list.forEach(function (interface) {
+            logger.debug("found interface", interface.name);
             if (interface.name == "ethwe") {
+                logger.info("selecting interface", interface);
                 deferred.resolve(interface.ip_address);
             }
         });
         network.get_active_interface(function (err, interface) {
+            logger.info("selecting interface", interface);
             deferred.resolve(interface.ip_address);
         })
     });
@@ -50,4 +51,5 @@ ClusterWatcher.prototype.register = function () {
 
 var config = yaml.load("config.yml");
 
-var clusterwatcher = new ClusterWatcher();
+var clusterwatcher = new ClusterWatcher(config);
+clusterwatcher.getAddress();
