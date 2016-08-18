b7bf0a3fc1b1291555415a82aad42549c02d23e4
Joshua Higgins <joshiggins@gmail.com>
2016-08-18 20:17:20 +0100

    add dns, watcher files

---
+++ clusterdns.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var os = require("os");
+var network = require("network");
+var promise = require("deferred");
+
+var logger = require("./log.js");
+var kvstore = require("./kvstore.js");
+
+var dgram = require('dgram');
+var ndns = require('./ndns.js');
+
+
+var ClusterDNS = function (config) {
+    // load the config file
+    this.config = config;
+    logger.info("ClusterDNS initialised with config", config);
+    // open kvstore
+    this.store = new kvstore(config);
+    // start up the server
+    this.server = ndns.createServer('udp4');
+    this.client = ndns.createClient('udp4');
+    this.server.on("request", (this.handleQuery).bind(this));
+    this.server.bind(53, "127.0.0.1");
+    //this.client.bind();
+}
+
+ClusterDNS.prototype.handleQuery = function (req, res) {
+    res.setHeader(req.header);
+    // check we only got 1 question in this query
+    if(req.q.length > 1) {
+        logger.warn("ClusterDNS got query with more than 1 question, rejecting");
+        res.send();
+        return;
+    }
+    // find the query name
+    res.addQuestion(req.q[0]);
+    var qname = req.q[0].name;
+    logger.debug("ClusterDNS got a query for", qname);
+    // look for record in kvstore for this name
+    var raddress = this.store.get("/cluster/"+this.config.cluster+"/hosts/"+qname);
+    if (!raddress) {
+        // see if address is a service name, and resolve to service host
+        console.log(this.store.list("/cluster/"+this.config.cluster+"/services"));
+        // else reject
+        logger.warn("ClusterDNS has no record for", qname, ", rejecting");
+        res.send();
+        return;
+    }
+    // prepare response
+    res.header.qr = 1;
+    res.header.ra = 1;
+    res.header.rd = 0;
+    res.header.ancount = 1;
+    res.header.nscount = 0;
+    res.header.arcount = 0;
+    res.addRR(qname, 1, "IN", "A", "10.10.10.10");
+
+    res.send();
+}
+
+module.exports = {
+    ClusterDNS: function (service, config, targets) {
+        var deferred = promise();
+        var clusterdns = new ClusterDNS(config.cluster);
+        deferred.resolve();
+        return deferred.promise();
+    }
+};
---
+++ clusterwatcher.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var os = require("os");
+var network = require("network");
+var promise = require("deferred");
+
+var logger = require("./log.js");
+var kvstore = require("./kvstore.js");
+
+
+var ClusterWatcher = function (config) {
+    // load the config file
+    this.config = config;
+    logger.info("ClusterWatcher initialised with config", config);
+    // open kvstore
+    this.store = new kvstore(config);
+}
+
+module.exports = {
+    ClusterWatcher: function (service, config, targets) {
+        var deferred = promise();
+        var clusterwatcher = new ClusterWatcher(config.cluster);
+        deferred.resolve();
+        return deferred.promise();
+    }
+};
