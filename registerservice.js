a924cd43d685537b2eda364ceabfc9f8b5ca21eb
Joshua Higgins <joshiggins@gmail.com>
2016-09-18 18:39:23 +0100

    registerservice.js stripped from clusternode.js

---
+++ registerservice.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var os = require("os");
+var network = require("network");
+var promise = require("deferred");
+
+var vccutil = require("./vccutil.js");
+var logger = require("./log.js");
+var kvstore = require("./kvstore.js");
+
+
+// register our service state if we are providing one
+var config = vccutil.getConfig();
+logger.debug(config);
+if(config.providers) {
+	// open kvstore
+	var store = new kvstore();
+	store.connect(config.kvstore.host, config.kvstore.port);
+	// register the service key
+	logger.debug("Registering service:", config.service);
+	store.register("/cluster/"+config.cluster+"/services/"+config.service, config.myhostname, 60);
+	logger.debug("service registered:", config.service)
+} else {
+	logger.info("There is no service to register.")
+}
