9cd46ccb884a81a3c6e0cc93e31fcf7be93afc95
Joshua Higgins <joshiggins@gmail.com>
2016-09-18 18:24:42 +0100

    loadtargets.js stripped from clusternode.js

---
+++ loadtargets.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var os = require("os");
+var os = require("fs");
+
+var vccutil = require("./vccutil.js");
+var logger = require("./log.js");
+
+
+// This is an init8js module, it runs in-process with the init in order to
+// register our cluster targets, so it must follow the service module pattern
+module.exports = {
+    LoadTargets: function (service, config, targets, f_register_services) {
+	    var deferred = promise();
+	    var config = vccutil.getConfig();
+	    var servicefile = "/etc/vcc/services-"+config.service+".yml";
+	    // make sure service targets definition file exists
+	    fs.stat(servicefile, function(err, stat) {
+	        if(err == null) {
+	            // parse the yaml file and register the targets
+	            logger.debug("Registering service provider targets for "+config.service);
+	            f_register_services(yaml.load(servicefile));
+	            deferred.resolve();
+	        } else if(err.code == 'ENOENT') {
+	            // no service file
+	            logger.error('There is no service definition for '+config.service);
+	            logger.error('Please create '+servicefile);
+	        } else {
+	            // something went wrong
+	            logger.error('unhandled error hook stat', servicefile);
+	        }
+	    });
+	    return deferred.promise();
+	}
+}
