7bd638d76b03b5267856dad6164cd002f5aa838b
Joshua Higgins <joshiggins@gmail.com>
2016-11-11 20:36:07 +0000

    kvstore is async+promise now. breaking everything.

--- kvstore.js
+++ kvstore.js
@@ -1,6 +1,8 @@
 var promise = require("deferred");
 var Etcd = require("node-etcd");
 var logger = require("winston");
+var path = require("path");
+
 
 function VccStore () {
 	this.connected = false;
@@ -13,93 +15,97 @@ VccStore.prototype.connect = function (host, port) {
 	this.connected = true;
 }
 
-VccStore.prototype.checkResponse = function (res) {
-	if (res.err) {
-		// key not found is acceptable
-		if(res.err.errorCode == 100) {
-			logger.debug("etcd error code was 100, key not found");
-			return undefined;
-		} else {
-			// otherwise, shout about the error
-			logger.error("got an error from etcd", res.err);
-			logger.error("error code was", res.err.errorCode);
-			logger.error("check connectivity and settings for kvstore server");
-			if (res.err.errors) {
-				for (var i = res.err.errors.length - 1; i >= 0; i--) {
-					console.log(res.err.errors[i]);
-				};
-			}
-			throw "got an error from etcd, throwing";
-		}
-	} else {
-		if (res.body) {
-			if (res.body.node) {
-				// res.body.node is what we want
-				return res.body.node;
-			} else {
-				logger.error("etcd response had body but no node");
-				console.log(res);
-				throw "etcd response had body but no node";
-			}
-		} else {
-			logger.error("etcd response had no body");
-			console.log(res);
-			throw "etcd response had no body";
-		}
-	}
-}
-
 VccStore.prototype.set = function (key, value, ttl) {
+	var deferred = promise();
 	if (!this.connected) {
-		throw "kvstore is not connected";
+		deferred.reject("not connected");
 	}
+	// set the key optionally with ttl
 	logger.debug("key set:", key, value, ttl);
+	var options = {};
 	if (ttl) {
-		logger.debug("before set with ttl");
-		var res = this.etcd.setSync(key, value, {ttl: ttl, maxRetries: 3});
-		logger.debug("after set with ttl");
-	} else {
-		logger.debug("before set");
-		var res = this.etcd.setSync(key, value);
-		logger.debug("after set");
+		options.ttl = ttl;
 	}
-	logger.debug("set return", res);
+	// call the etcd function
+	this.etcd.set(key, value, options, function (err, res) {
+		if (err) {
+			deferred.reject(err);
+		} else {
+			deferred.resolve();
+		}
+	});
+	// return promise
+	return deferred.promise();
 }
 
-VccStore.prototype.get = function (key) {
+VccStore.prototype.get = function (key, recursive) {
+	var deferred = promise();
+	var me = this;
 	if (!this.connected) {
-		throw "kvstore is not connected";
+		deferred.reject("not connected");
 	}
-	logger.debug("key get:", key)
-	var keynode = this.checkResponse(this.etcd.getSync(key));
-	if (keynode) {
-		return keynode.value;
+	logger.debug("key get:", key, recursive);
+	var options = {};
+	if (recursive) {
+		options.recursive = true;
 	}
-	return keynode;
+	// call the etcd function
+	this.etcd.get(key, function (err, res) {
+		if (err) {
+			deferred.reject(err);
+		} else {
+			// check the response
+			if (res.node) {
+				if (recursive) {
+					if (res.node.nodes) {
+						// convert a list of objects into a list
+						// only show the base name of the full path
+						var listresult = res.node.nodes.reduce(function (r, i) {
+							r.push(path.basename(i.key));
+							return r;
+						}, []);
+						deferred.resolve(listresult);
+					} else {
+						deferred.reject("recursive, expecting nodes but didn't get any");
+					}
+				} else {
+					if (res.node.dir) {
+						deferred.reject(res.node.key + " is a directory");
+					} else if (res.node.value) {
+						deferred.resolve(res.node.value);
+					} else {
+						deferred.reject("no value in response");
+					}
+				}
+			} else {
+				deferred.reject("no node in response");
+			}
+		}
+	});
+	// return promise
+	return deferred.promise();
 }
 
 VccStore.prototype.watch = function (key) {
+	var deferred = promise();
 	if (!this.connected) {
-		throw "kvstore is not connected";
+		deferred.reject("not connected");
 	}
 	// returns an event emitter on change
 	logger.debug("key watch:", key);
-	return this.etcd.watcher(key);
+	deferred.resolve(this.etcd.watcher(key));
+	// return promise
+	return deferred.promise();
 }
 
 VccStore.prototype.list = function (key) {
-	if (!this.connected) {
-		throw "kvstore is not connected";
-	}
-	logger.debug("key list:", key);
-	var keynode = this.checkResponse(this.etcd.getSync(key, {recursive: true}));
-	if (keynode) {
-		return keynode.nodes;
-	}
-	return keynode;
+	// call the get function with recursive option
+	return this.get(key, true);
 }
 
 VccStore.prototype.register = function (key, value, ttl) {
+	// this is the only function that does not return a promise
+	// because there is no obvious resolution
 	if (!this.connected) {
 		throw "kvstore is not connected";
 	}
