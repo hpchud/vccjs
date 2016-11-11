6839584474ef2cc24c66f516b16ab807dc0509c5
Joshua Higgins <joshiggins@gmail.com>
2016-11-11 14:01:29 +0000

    promisify clusternet.js

--- clusternet.js
+++ clusternet.js
@@ -7,40 +7,26 @@ var promise = require("deferred");
 var vccutil = require("./vccutil.js");
 var logger = require("./log.js");
 
+// convert async network functions to promises
+var getInterfacesList = promise.promisify(network.get_interfaces_list);
+var getActiveInterface = promise.promisify(network.get_active_interface);
 
 var getAddress = function () {
     var deferred = promise();
-    network.get_interfaces_list(function (err, list) {
-        if(err) {
-            logger.error(err);
-            deferred.reject();
-            return;
-        }
-        list.forEach(function (interface) {
+    // run both get interfaces and get active interface in a promise group
+    // this means we avoid nesting promises which is ugly
+    promise(getInterfacesList(), getActiveInterface())(function (result) {
+        // check for weave
+        result[0].forEach(function (interface) {
             logger.debug("found interface", interface.name);
             if (interface.name == "ethwe") {
                 logger.debug("selecting interface", interface);
                 deferred.resolve(interface.ip_address);
             }
         });
-        network.get_active_interface(function (err, interface) {
-            if (err) {
-                logger.warn(err);
-                if (list.length > 0) {
-                    logger.warn("This could be because we are in Docker");
-                    logger.warn("ClusterNet will use the first available interface");
-                    logger.debug("selecting interface", list[0]);
-                    deferred.resolve(list[0].ip_address);
-                    return;
-                } else {
-                    logger.error("There are no network interfaces");
-                    deferred.reject();
-                    return;
-                }
-            }
-            logger.debug("selecting interface", interface);
-            deferred.resolve(interface.ip_address);
-        })
+        // otherwise use active interface
+        logger.debug("selecting interface", result[1]);
+        deferred.resolve(result[1].ip_address);
     });
     return deferred.promise();
 }
