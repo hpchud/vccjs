dd09f553f4c740a999db80cd5ad0cd30937eb42f
Joshua Higgins <joshiggins@gmail.com>
2017-01-06 15:13:11 +0000

    refactor tool.js - just handle vcc options\ncontainer runtime helpers will be in another wrapper outside the container....

--- tool.js
+++ tool.js
@@ -1,25 +1,17 @@
 #!/usr/bin/env node
 
 var yaml = require("yamljs");
+var glob = require('glob');
 
 // define the required arguments
 opt = require('node-getopt').create([
-  ['', 'start', 'start the image'],
   ['', 'cluster=NAME', 'name of cluster to join'],
-  ['', 'start-storage', 'start a storage service for this vcc, otherwise, use --storage-[host,port] option'],
+  ['', 'storage-type=TYPE', 'storage service type, supports: etcd'],
   ['', 'storage-host=IP', 'ip address of storage host'],
   ['', 'storage-port=PORT', 'port of storage service'],
   ['', 'service=SERVICE', 'for a multi service image, specify the service to start'],
   ['', 'force-address=IP', 'manually set the advertised IP of this instance'],
   ['', 'no-dns', 'don\'t use the ClusterDNS service'],
-  ['', 'privileged', 'run the containers in privileged mode (e.g. for NFS)'],
-  ['', 'hostnet', 'use the host networking model'],
-  ['', 'name', 'give container the name vcc-[cluster]-[service] (useful for scripting)'],
-  ['', 'bind=PATH', 'bind mount folder from host into container'],
-  ['', 'restart', 'set the Docker option to automatically restart container'],
-  ['', 'justdoit', 'set options privileged, hostnet, name, bind=/home, restart'],
-  ['', 'swarm', 'specify the standalone Docker Swarm to use for provisioning'],
-  ['', 'swarm-node-exclusive', 'only provision one container per node for this cluster in the swarm'],
   //['', 'eval', 'format output suitable to eval in the host shell'],
   ['', 'just-yml', 'just dump the generated cluster init.yml and nothing else'],
   ['i', 'info', 'display information about this vcc image'],
@@ -34,24 +26,10 @@ var options = opt.options;
 
 //console.log(options);
 
-if ((options.version || options.info || options.start)) {
+if ((options.version || options.info)) {
     // version command
     if (options.version) {
-        console.log("vcc version 0.1");
-        /*console.log("                                _");
-        console.log("                               | \\");
-        console.log("  meow                         | |");
-        console.log("                               | |");
-        console.log("           |\\                  | |");
-        console.log("          /, ~\                / /");
-        console.log("         X     `-.....-------./ /");
-        console.log("          ~-. ~  ~              |");
-        console.log("             \\             /    |");
-        console.log("              \\  /_     ___\\   /");
-        console.log("              | /\\ ~~~~~   \\  |");
-        console.log("              | | \\        || |");
-        console.log("              | |\\ \\       || )");
-        console.log("             (_/ (_/      ((_/");*/
+        console.log("vccjs tool 0.1");
         console.log("▒▒▒▒▒▒▒▓");
         console.log("▒▒▒▒▒▒▒▓▓▓");
         console.log("▒▓▓▓▓▓▓░░░▓");
@@ -74,92 +52,75 @@ if ((options.version || options.info || options.start)) {
     }
     // info command
     if (options.info) {
-
-    }
-    // generate command for starting the storage if we need to
-    if (options['start-storage']) {
-        var storagecommand = "docker run -d ";
-        if (options.hostnet) {
-            storagecommand += "--net=host ";
-        }
-        if (options.name) {
-            storagecommand += "--name=vcc-"+options.cluster+"-kvstore ";
-        }
-        storagecommand += "quay.io/coreos/etcd:v3.0.8 /usr/local/bin/etcd --listen-client-urls 'http://0.0.0.0:2379,http://0.0.0.0:4001' --advertise-client-urls 'http://0.0.0.0:2379,http://0.0.0.0:4001'";
-        console.log(storagecommand);
-    }
-    // start command, the good stuff happens here
-    if (options.start) {
-        // check cluster name is specified
-        if (!options.cluster) {
-            console.error("You must specify a cluster name");
-            process.exit(1);
-        }
-        if (options.justdoit) {
-            options.privileged = true;
-            options.hostnet = true;
-            options.name = true;
-            options.bind = "/home";
-            options.restart = true;
-        }
-        // check we have either --start-storage or --storage-host and --storage-port
-        if (!options['start-storage']) {
-            if (!(options['storage-host'] && options['storage-port'])) {
-                console.error("You must specify either --start-storage OR --storage-host and --storage-port");
-            }
-        }
-        // generate yml config with cluster name and storage details in
-        // since the tool is executed within the image itself, we can just look at /etc/init.yml
+        // get some basic details about this image
         var inityml = yaml.load("/etc/init.yml");
-        inityml.cluster.kvstore.type = "etcd";
-        if (!options['start-storage']) {
-            inityml.cluster.kvstore.host = options['storage-host'];
-            inityml.cluster.kvstore.port = options['storage-port'];
+        var info = {};
+        if(!inityml.cluster.image_info) {
+            console.error("This image does not define any author info in the init.yml");
         } else {
-            inityml.cluster.kvstore.host = "localhost";
-            inityml.cluster.kvstore.port = "4001";
+            info['Name'] = inityml.cluster.image_info.name;
+            info['Version'] = inityml.cluster.image_info.version;
+            info['Author'] = inityml.cluster.image_info.author;
         }
-        if(options['force-address']) {
-            inityml.cluster.myaddress = options['force-address'];
-        }
-        if(options['no-dns']) {
-            inityml.cluster.nodns = true;
-        }
-        inityml.cluster.cluster = options.cluster;
-        if (options.service) {
-            inityml.cluster.service = options.service;
-        }
-        var b64inityml = new Buffer(yaml.stringify(inityml)).toString('base64');
-        // generate command for starting this image
-        if (options.swarm) {
-            var runcommand = "docker -H "+options.swarm+" run -d ";
-        } else {
-            var runcommand = "docker run -d ";
-        }
-        if (options.privileged) {
-            runcommand += "--privileged ";
-        }
-        if (options.hostnet) {
-            runcommand += "--net=host ";
-        }
-        if (options['swarm-node-exclusive']) {
-            runcommand += '--label="vcc'+options.cluster+'" -e "affinity:container!=*vcc'+options.cluster+'*" ';
-        }
-        if (options.name) {
-            runcommand += "--name=vcc-"+options.cluster+"-"+options.service+" ";
-        }
-        if (options.bind) {
-            runcommand += "-v "+options.bind+":"+options.bind+" ";
-        }
-        runcommand += "$VCCIMAGE b64inityml="+b64inityml;
-        // output the commands in the desired format
-        if (options['just-yml']) {
-            console.log(yaml.stringify(inityml));
-        } else {
-            console.log(runcommand);
-        }
+        // check for multi service image
+        info['Services'] = {};
+        info.Services['Default'] = inityml.cluster.service;
+        info.Services['Available'] = [];
+        files = glob.sync("/etc/vcc/services-*.yml");
+        for (var i = files.length - 1; i >= 0; i--) {
+            info.Services.Available.push(files[i].replace("/etc/vcc/services-", "").replace(".yml", ""));
+        };
+        
+        // check for service providers and dependencies
+        var deps = yaml.load("/etc/vcc/dependencies.yml");
+        info.Services['Dependencies'] = deps;
+        // print out in yaml
+        console.log(yaml.stringify(info, 8));
     }
 } else {
-    console.error("You must either start the image or use one of the [info|help|version] options");
-    process.exit(1);
+    // check cluster name is specified
+    if (!options.cluster) {
+        console.error("You must specify a cluster name using --cluster=NAME");
+        process.exit(1);
+    }
+    // check we have either --start-storage or --storage-host and --storage-port
+    if (!(options['storage-host'] && options['storage-port'])) {
+        console.error("You must specify --storage-host and --storage-port");
+        process.exit(1);
+    }
+    // generate yml config with cluster name and storage details in
+    // since the tool is executed within the image itself, we can just look at /etc/init.yml
+    var inityml = yaml.load("/etc/init.yml");
+    // storage settings
+    if(!options['storage-type']) {
+        inityml.cluster.kvstore.type = "etcd";
+    } else {
+        inityml.cluster.kvstore.type = options['storage-type'];
+    }
+    inityml.cluster.kvstore.host = options['storage-host'];
+    inityml.cluster.kvstore.port = options['storage-port'];
+    // address override
+    if(options['force-address']) {
+        inityml.cluster.myaddress = options['force-address'];
+    }
+    // cluster dns
+    if(options['no-dns']) {
+        inityml.cluster.nodns = true;
+    }
+    // set cluster name and service
+    inityml.cluster.cluster = options.cluster;
+    if (options.service) {
+        inityml.cluster.service = options.service;
+    }
+    // convert to yaml and then base64 encode
+    var b64inityml = new Buffer(yaml.stringify(inityml)).toString('base64');
+    // generate command for starting this image
+    var runcommand = "/vccjs/launch.sh ";
+    runcommand += "b64inityml="+b64inityml;
+    // output the commands in the desired format
+    if (options['just-yml']) {
+        console.log(yaml.stringify(inityml));
+    } else {
+        console.log(runcommand);
+    }
 }
