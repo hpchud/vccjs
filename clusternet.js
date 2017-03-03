ef0c05c0f5e9b2e3c4bf329bd0093c7f66fc2022
Joshua Higgins <joshiggins@gmail.com>
2017-03-03 16:47:26 +0000

    refactor into prototypes to support automated testing and module exporting

--- clusternet.js
+++ clusternet.js
@@ -11,12 +11,40 @@ var logger = require("./log.js");
 var getInterfacesList = promise.promisify(network.get_interfaces_list);
 var getActiveInterface = promise.promisify(network.get_active_interface);
 
-var config = null;
 
-// if an address is already set, we see if an interface is available with that address
-// if not, we override the manually set address with our discovered one
+/**
+ * Represents a VCC container's network configuration
+ * @constructor
+ * @param {Object} config - A configuration object (usually loaded from vccutil.GetConfig)
+ */
+var ClusterNet = function (config) {
+    this.config = config;
+    this.interfaces_list = null;
+    this.active_interface = null;
+    this.name_to_ip = null;
+    this.ip_to_name = null;
+};
 
-var filterInterface = function (i) {
+ClusterNet.prototype.getInterfaces = function () {
+    var deferred = promise();
+    var me = this;
+    getInterfacesList().then(function (interfaces_list) {
+        me.interfaces_list = interfaces_list;
+        getActiveInterface().then(function (active_interface) {
+            me.active_interface = active_interface;
+            deferred.resolve();
+        }, function (err) {
+            me.active_interface = {};
+            deferred.resolve();
+        });
+    }, function (err) {
+        me.interfaces_list = [];
+        deferred.resolve();
+    });
+    return deferred.promise();
+}
+
+ClusterNet.prototype.filterInterface = function (i) {
     if (i.name.startsWith("docker")) {
         return true;
     } else if (i.name.startsWith("dummy")) {
@@ -26,83 +54,138 @@ var filterInterface = function (i) {
     }
 }
 
-var getAddress = function () {
-    var deferred = promise();
-
-    getInterfacesList().then(function (interfaces_list) {
-        // convert interfaces list into something we can work with
-        var name_to_ip = interfaces_list.reduce(function (r, i) {
-            if (!filterInterface(i)) {
-                if (i.ip_address) {
-                    r[i.name] = i.ip_address;
-                }
-            }
-            return r;
-        }, {});
-        var ip_to_name = interfaces_list.reduce(function (r, i) {
-            if (!filterInterface(i)) {
-                if (i.ip_address) {
-                    r[i.name] = i.ip_address;
-                }
-            }
-            return r;
-        }, {});
-        // see if there is an address already set, and if so, is there an interface for it
-        if(config.myaddress) {
-            logger.debug("there is a saved address", config.myaddress);
-            if(config.myaddress in ip_to_name) {
-                logger.debug("found interface for saved address", ip_to_name[config.myaddress]);
-                deferred.resolve(config.myaddress);
-            } else {
-                logger.warn("there is no interface for saved address, ignoring it...");
+ClusterNet.prototype.parseInterfacesList = function () {
+    var me = this;
+    // convert interfaces list into something we can work with
+    var name_to_ip = this.interfaces_list.reduce(function (r, i) {
+        if (!me.filterInterface(i)) {
+            if (i.ip_address) {
+                r[i.name] = i.ip_address;
             }
         }
-        // see if there is a weave interface
-        if('ethwe' in name_to_ip) {
-            logger.debug("found weave interface");
-            deferred.resolve(name_to_ip['ethwe']);
+        return r;
+    }, {});
+    var ip_to_name = this.interfaces_list.reduce(function (r, i) {
+        if (!me.filterInterface(i)) {
+            if (i.ip_address) {
+                r[i.name] = i.ip_address;
+            }
+        }
+        return r;
+    }, {});
+    this.name_to_ip = name_to_ip;
+    this.ip_to_name = ip_to_name;
+    return [name_to_ip, ip_to_name];
+}
+
+ClusterNet.prototype.hasSavedAddress = function () {
+    // if an address is already set, we see if an interface is available with that address
+    // if not, we override the manually set address with our discovered one
+    if(this.config.myaddress) {
+        logger.debug("there is a saved address", this.config.myaddress);
+        if(this.config.myaddress in this.ip_to_name) {
+            logger.debug("found interface for saved address", this.ip_to_name[this.config.myaddress]);
+            return this.config.myaddress;
         } else {
-            logger.debug("there is no weave interface");
+            logger.warn("there is no interface for saved address, ignoring it...");
+            return false;
         }
-        // detect the active interface
-        getActiveInterface().then(function (active_interface) {
-            logger.debug("active interface is", active_interface.name);
-            deferred.resolve(active_interface.ip_address);
-        }, function (err) {
-            logger.warn("could not determine active interface");
-            // make sure we pick only from interfaces with ips, sorted alphabetically
-            // since en* will come before any l* or v* interfaces
-            var firstint = Object.keys(name_to_ip).sort()[0];
-            logger.warn("using first available interface", firstint);
-            deferred.resolve(name_to_ip[firstint]);
-        });
-    });
+    } else {
+        logger.debug("there is no saved address");
+        return false;
+    }
+}
 
+ClusterNet.prototype.hasWeaveInterface = function () {
+    // see if there is a weave interface
+    if('ethwe' in this.name_to_ip) {
+        logger.debug("found weave interface");
+        return this.name_to_ip['ethwe'];
+    } else {
+        logger.debug("there is no weave interface");
+        return false;
+    }
+}
+
+ClusterNet.prototype.hasActiveInterface = function () {
+    // detect the active interface
+    if (this.active_interface) {
+        logger.debug("active interface is", this.active_interface.name);
+        return this.active_interface.ip_address;
+    } else {
+        logger.warn("could not determine active interface");
+        return false;
+    }
+}
+
+ClusterNet.prototype.hasAvailableInterface = function () {
+    // return first available interface
+    if (Object.keys(this.name_to_ip).length > 0) {
+        var firstint = Object.keys(this.name_to_ip).sort()[0];
+        logger.warn("using first available interface", firstint);
+        return this.name_to_ip[firstint];
+    } else {
+        logger.error("there are no interfaces available");
+        return false;
+    }
+}
+
+ClusterNet.prototype.getAddress = function () {
+    // try our options
+    var ip = this.hasSavedAddress();
+    if(ip) {
+        return ip;
+    }
+    ip = this.hasWeaveInterface();
+    if(ip) {
+        return ip;
+    }
+    ip = this.hasActiveInterface();
+    if(ip) {
+        return ip;
+    }
+    ip = this.hasAvailableInterface();
+    if(ip) {
+        return ip;
+    }
+    logger.error("could not get an address");
+    return false;
+}
+
+ClusterNet.prototype.writeConfig = function (address) {
+    var deferred = promise();
+    logger.info("our IP address is", address);
+    logger.info("our hostname is", os.hostname());
+    // update the config file with our address and hostname
+    this.config.myaddress = address;
+    this.config.myhostname = os.hostname();
+    logger.debug("going to write config");
+    vccutil.writeConfig(this.config).then(function () {
+        logger.info("updated config");
+        deferred.resolve();
+    },
+    function (err) {
+        logger.error("failed to write config");
+        logger.error(err);
+        deferred.reject(err);
+    });
     return deferred.promise();
+};
+
+ClusterNet.prototype.main = function () {
+    var me = this;
+    // get the interfaces
+    this.getInterfaces().then(function () {
+        // parse the interface list
+        me.parseInterfacesList();
+        // write the discovered address
+        me.writeConfig(me.getAddress());
+    });
 }
 
 if (require.main === module) {
-
-    config = vccutil.getConfig();
-    logger.debug("current config is", config);
-
-    getAddress().then(function (address) {
-        logger.info("our IP address is", address);
-        logger.info("our hostname is", os.hostname());
-        // update the config file with our address and hostname
-        config.myaddress = address;
-        config.myhostname = os.hostname();
-        logger.debug("going to write config");
-        vccutil.writeConfig(config).then(function () {
-            logger.info("updated config");
-        },
-        function (err) {
-            logger.error("failed to write config");
-            logger.error(err);
-        });
-    }).done();
+    var clusternet = new ClusterNet(vccutil.getConfig());
+    clusternet.main();
 } else {
-    exports.config = config;
-    exports.filterInterface = filterInterface;
-    exports.getAddress = getAddress;
+    module.exports = ClusterNet;
 }
