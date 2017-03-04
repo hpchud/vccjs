9433e441e236f2e1e62e1be972a133ed118dfc05
Joshua Higgins <joshiggins@gmail.com>
2017-03-04 16:17:55 +0000

    refactor registerservice

--- registerservice.js
+++ registerservice.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var os = require("os");
-var network = require("network");
 var promise = require("deferred");
 
 var vccutil = require("./vccutil.js");
@@ -9,17 +8,28 @@ var logger = require("./log.js");
 var kvstore = require("./kvstore.js");
 
 
-// register our service state if we are providing one
-var config = vccutil.getConfig();
-logger.debug(config);
-if(config.providers) {
-	// open kvstore
-	var kv = new kvstore();
-	kv.connect(config.kvstore.host, config.kvstore.port);
-	// register the service key
-	logger.debug("Registering service:", config.service);
-	kv.register("/cluster/"+config.cluster+"/services/"+config.service, config.myhostname, 60);
-	logger.debug("service registered:", config.service)
-} else {
-	logger.info("There is no service to register.")
+exports.registerService = function (config) {
+    var deferred = promise();
+    // register our service state if we are providing one
+    if(config.providers) {
+        // open kvstore
+        var kv = new kvstore();
+        kv.connect(config.kvstore.host, config.kvstore.port);
+        // register the service key
+        logger.debug("Registering service:", config.service);
+        kv.register("/cluster/"+config.cluster+"/services/"+config.service, config.myhostname, 60).then(function () {
+            logger.debug("service registered:", config.service);
+            deferred.resolve(true);
+        }, function (err) {
+            deferred.reject(err);
+        });
+    } else {
+        logger.info("There is no service to register.")
+        deferred.resolve(false);
+    }
+    return deferred.promise();
+}
+
+if (require.main === module) {
+    exports.registerService(vccutil.getConfig());
 }
