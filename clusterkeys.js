4e3e2c4e1419a0bb06b520e2c295acfd57bdd157
Joshua Higgins <joshiggins@gmail.com>
2017-11-09 08:27:17 +0000

    generate keys function

--- clusterkeys.js
+++ clusterkeys.js
@@ -2,6 +2,9 @@
 
 var os = require("os");
 var promise = require("deferred");
+var keygen = require('ssh-keygen');
+var fs = require('fs');
+var path = require('path');
 
 var vccutil = require("./vccutil.js");
 var logger = require("./log.js");
@@ -13,6 +16,8 @@ var logger = require("./log.js");
  */
 var ClusterKeys = function (config) {
     this.config = config;
+    this.current_user = os.userInfo().username;
+    this.current_home = os.userInfo().homedir;
 };
 
 /**
@@ -33,7 +38,41 @@ ClusterKeys.prototype.writeAuthorizedKeys = function () {
  * Generate keys if they don't exist for the current user
  */
 ClusterKeys.prototype.generateKeys = function () {
+    var deferred = promise();
     var me = this;
+    var location = path.join(me.current_home, ".ssh", "id_rsa");
+    var comment = '@vcc';
+    var password = false;
+
+    // does key exist?
+    fs.stat(location, function(err, stat) {
+        if(err == null) {
+            logger.info("Private key already exists for", me.current_user);
+            deferred.resolve();
+        } else if(err.code == 'ENOENT') {
+            // file does not exist
+            logger.debug('Private key does not exist for', me.current_user, "ENOENT");
+            logger.info("Start generating keys for", me.current_user);
+            logger.info("This could take a while...");
+            keygen({
+                location: location,
+                comment: comment,
+                password: password,
+                read: false
+            }, function(err){
+                if (err) {
+                    logger.error("Something went wrong generating keys:", err);
+                    deferred.reject();
+                }
+                logger.info("Finished generating keys for", me.current_user);
+            });
+        } else {
+            logger.error('Error testing for private key for', me.current_user, err.code);
+            deferred.reject();
+        }
+    });
+
+    return deferred.promise();
 }
 
 /**
