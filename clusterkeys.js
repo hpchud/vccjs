4d245758c40be64cf55a0661f6751b3178c94011
Joshua Higgins <joshiggins@gmail.com>
2017-11-09 08:05:48 +0000

    add functions to implement

--- clusterkeys.js
+++ clusterkeys.js
@@ -7,13 +7,48 @@ var vccutil = require("./vccutil.js");
 var logger = require("./log.js");
 
 /**
- * Represents a VCC container's network configuration
+ * Public key discovery for the VCC
  * @constructor
  * @param {Object} config - A configuration object (usually loaded from vccutil.GetConfig)
  */
 var ClusterKeys = function (config) {
     this.config = config;
 };
+
+/**
+ * Enumerate all public keys in the discovery
+ */
+ClusterKeys.prototype.enumeratePublicKeys = function () {
+    var me = this;
+}
+
+/**
+ * Write an authorized_keys file for the current user
+ */
+ClusterKeys.prototype.writeAuthorizedKeys = function () {
+    var me = this;
+}
+
+/**
+ * Generate keys if they don't exist for the current user
+ */
+ClusterKeys.prototype.generateKeys = function () {
+    var me = this;
+}
+
+/**
+ * Publish my key to the discovery service
+ */
+ClusterKeys.prototype.publishKeys = function () {
+    var me = this;
+}
+
+/**
+ * The loop to watch the discovery KV store for changes
+ */
+ClusterKeys.prototype.watchCluster = function () {
+    var me = this;
+}
 
 /**
  * The main function to call if we are loaded as a daemon
