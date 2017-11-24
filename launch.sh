2590559caf9a379b0e8233ccd6c2e8dabb19df7d
Joshua Higgins <joshiggins@gmail.com>
2017-11-24 16:09:37 +0000

    fix launch.sh to work with new tool

--- launch.sh
+++ launch.sh
@@ -3,10 +3,11 @@
 # run the tool
 tooloutput=$(/vccjs/tool.js $@)
 if [ "$?" = "0" ]; then
-	# if return code is 0, run the init8js daemon to start
-	exec $tooloutput
+	# if return code is 0, write the cluster.yml and start init
+	echo $tooloutput > /run/cluster.yml
+	exec /usr/sbin/init
 else
-	# otherwise, tool just printed informational messages
+	# otherwise, tool just print the output
 	echo "$tooloutput"
 	exit 1
 fi
